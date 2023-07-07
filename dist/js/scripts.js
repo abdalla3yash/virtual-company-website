09dbbf0b343823752af5124387b6c1f4394b248e
abdalla3yash <abdo3yash@gmail.com>
2023-07-07 17:54:34 +0300

    first commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Coming Soon v6.0.7 (undefined)
+* Copyright 2013-2023 undefined
+* Licensed under undefined (https://github.com/StartBootstrap/virtual-company/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
